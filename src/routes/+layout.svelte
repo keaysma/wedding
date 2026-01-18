0901e57221859f4beab3087a2c608b33386a3c66
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2026-01-17 23:32:59 -0500

    drop unused fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,24 +8,12 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<style>
-	@import url('https://fonts.googleapis.com/css2?family=Fleur+De+Leah&family=Imperial+Script&family=Mrs+Saint+Delafield&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Fleur+De+Leah&display=swap');
 
 	.fleur-de-leah-regular {
 		font-family: "Fleur De Leah", cursive;
 		font-weight: 400;
 		font-style: normal;
-	}
-
-	.imperial-script-regular {
-		font-family: "Imperial Script", cursive;
-		font-weight: 400;
-		font-style: normal;
-	}
-
-	.mrs-saint-delafield-regular {
-  		font-family: "Mrs Saint Delafield", cursive;
-  		font-weight: 400;
-  		font-style: normal;
 	}
 
 	@font-face {
